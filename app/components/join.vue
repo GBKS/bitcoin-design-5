924f8bce015e56fb1c49a117eee75b7708bb9ee9
GBKS <chri@sto.ph>
2025-08-19 13:34:36 +0200

    Join section copy tweaks

--- app/components/join.vue
+++ app/components/join.vue
@@ -23,17 +23,23 @@
         href="https://discord.gg/K7aQ5PErht"
         rel="me"
         target="_blank" 
-      >Chat on Discord</a>
+      >Come say hi <svg viewBox="0 0 24 24" role="img" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M20.317 4.37a19.791 19.791 0 0 0-4.885-1.515.074.074 0 0 0-.079.037c-.21.375-.444.864-.608 1.25a18.27 18.27 0 0 0-5.487 0 12.64 12.64 0 0 0-.617-1.25.077.077 0 0 0-.079-.037A19.736 19.736 0 0 0 3.677 4.37a.07.07 0 0 0-.032.027C.533 9.046-.32 13.58.099 18.057a.082.082 0 0 0 .031.057 19.9 19.9 0 0 0 5.993 3.03.078.078 0 0 0 .084-.028 14.09 14.09 0 0 0 1.226-1.994.076.076 0 0 0-.041-.106 13.107 13.107 0 0 1-1.872-.892.077.077 0 0 1-.008-.128 10.2 10.2 0 0 0 .372-.292.074.074 0 0 1 .077-.01c3.928 1.793 8.18 1.793 12.062 0a.074.074 0 0 1 .078.01c.12.098.246.198.373.292a.077.077 0 0 1-.006.127 12.299 12.299 0 0 1-1.873.892.077.077 0 0 0-.041.107c.36.698.772 1.362 1.225 1.993a.076.076 0 0 0 .084.028 19.839 19.839 0 0 0 6.002-3.03.077.077 0 0 0 .032-.054c.5-5.177-.838-9.674-3.549-13.66a.061.061 0 0 0-.031-.03zM8.02 15.33c-1.183 0-2.157-1.085-2.157-2.419 0-1.333.956-2.419 2.157-2.419 1.21 0 2.176 1.096 2.157 2.42 0 1.333-.956 2.418-2.157 2.418zm7.975 0c-1.183 0-2.157-1.085-2.157-2.419 0-1.333.955-2.419 2.157-2.419 1.21 0 2.176 1.096 2.157 2.42 0 1.333-.946 2.418-2.157 2.418z" fill="currentColor"/></svg></a>
 
-      <p>Some things to do:</p>
-      <ol>
-        <li>Join Discord Discussions</li>
-        <li>Explore the Bitcoin Design Guide</li>
-        <li>Jump on a Community Call</li>
-        <li>Contribute to a Design Review</li>
-        <li>Attend an event</li>
-        <li>Apply for a Grant</li>
-      </ol>
+      <p>Start here:</p>
+      <ul>
+        <li>
+          <a href="https://bitcoin.design/guide/" target="_blank">Explore the Design Guide:</a><br/>Everything we know about Bitcoin UX in one place
+        </li>
+        <li>
+          <a href="https://bitcoin.design/guide/resources/learning-bootcamp/" target="_blank">Try the Learning Bootcamp:</a><br/>Four weeks, go from curious to contributing
+        </li>
+        <li>
+          <a href="https://github.com/BitcoinDesign/Guide/issues" target="_blank">Review someone's work:</a><br/>Your outside perspective is valuable
+        </li>
+        <li>
+          <a href="https://bitcoin.design/calendar/" target="_blank">Jump on a community call:</a><br/>Real people talking about real design problems
+        </li>
+      </ul>
 
       <a
         class="-button --outline --black"
@@ -82,17 +88,34 @@
       text-wrap: balance;
     }
 
-    ol {
+    ul {
       margin-top: 10px;
+      margin-bottom: 0;
       
       li {
-        font-size: 21px;
+        font-size: 19px;
         line-height: 1.4;
+
+        span,
+        a {
+          font-weight: 600;
+          color: black;
+        }
+
+        a {
+          &:hover {
+            color: var(--blue);
+          }
+        }
+
+        & + li {
+          margin-top: 10px;
+        }
       }
     }
 
-    a {
-      margin-top: 40px;
+    > a {
+      margin-top: 30px;
       display: inline-block;
 
       & + p {
